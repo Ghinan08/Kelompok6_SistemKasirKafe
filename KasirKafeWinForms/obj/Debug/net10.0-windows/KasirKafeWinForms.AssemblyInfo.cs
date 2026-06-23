a004ec7ca5a3ac0b923b9eb4e8f62e75af12e676
okdokie123 <erland.adinata@gmail.com>
2026-06-23 21:17:48 +0700

    fix FormDapur v2

--- KasirKafeWinForms/obj/Debug/net10.0-windows/KasirKafeWinForms.AssemblyInfo.cs
+++ KasirKafeWinForms/obj/Debug/net10.0-windows/KasirKafeWinForms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KasirKafeWinForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67f2cf5f9c869cbdae85e74ded96f5bed3aca02a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49925cb3b652e18c27aa66ee586a5751814cf6fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("KasirKafeWinForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KasirKafeWinForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- KasirKafe_Kel6/obj/Debug/net10.0/KasirKafe_Kel6.AssemblyInfo.cs
+++ KasirKafe_Kel6/obj/Debug/net10.0/KasirKafe_Kel6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KasirKafe_Kel6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67f2cf5f9c869cbdae85e74ded96f5bed3aca02a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49925cb3b652e18c27aa66ee586a5751814cf6fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("KasirKafe_Kel6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KasirKafe_Kel6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
